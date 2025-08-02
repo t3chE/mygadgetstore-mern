e23db07226daa0586939dfb9a1bb1664819f34f6
t3chE <t0dtym3@gmail.com>
2025-08-02 12:43:23 +0100

    NotFoundPage.jsx added

---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFoundPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      {/* You can add a link back to the homepage here */}
+    </div>
+  );
+}
+
+export default NotFoundPage;
